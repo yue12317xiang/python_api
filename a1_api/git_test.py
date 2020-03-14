59ee0a52ef4ad743860d364ad7ff2427c5fdd3da
12317xiang <15119724465@qq.com>
2020-03-14 22:53:09 +0800

    update

--- a1_api/git_test.py
+++ a1_api/git_test.py
@@ -1,11 +1,6 @@
 def funcname(self,parameter_list,name):
     self.name = "yue tuo shi"
-    print(parameter_list)
-<<<<<<< HEAD
     print("你的名字是：{0}".format(name))
-funcname(123)
-=======
 def add(num1,num2):
     return num1+num2
 funcname(add(1,2))
->>>>>>> b93ce253cbce88a5ba1636f2ca644b27ffdd32c1
